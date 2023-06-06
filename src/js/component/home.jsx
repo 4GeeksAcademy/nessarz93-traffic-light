b581672e8098c673811b355ff943dcba7a8d22c1
nessarz93 <126210620+nessarz93@users.noreply.github.com>
2023-06-06 00:24:14 +0000

    inicio de diseño

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,20 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Light from "./light.js";
 
 //create your first component
 const Home = () => {
+	const colors = ["red", "yellow", "green"]
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="poste">
 		</div>
+		<div className="semaforo">
+			{colors.map((color) => {
+				return <Light/>;
+			})}
+		</div>
+		</>
 	);
 };
 
---
+++ src/js/component/light.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Light = () => {
+return (
+    <>
+        <div className="luz">
+            <p>light</p>
+        </div>
+    </>
+)
+}
+
+export default Light;
